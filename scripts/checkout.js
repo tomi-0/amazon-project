24e9b0108b69c26771cc5d86fbc23df36a7559a9
tomi-0 <asadetomi@gmail.com>
2025-08-25 18:56:06 +0100

    fixed price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 //import {cart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 
 let cart = [{
@@ -38,7 +39,7 @@ cart.forEach( cartItem => {
                   ${productElement.name}
                 </div>
                 <div class="product-price">
-                  ${productElement.priceCents/100}
+                  ${formatCurrency(productElement.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -60,7 +61,7 @@ cart.forEach( cartItem => {
 
                 <div class="delivery-option">
                   <input type="radio" class="delivery-option-input"
-                    name="delivery-option-2">
+                    name="delivery-option-${productElement.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -72,7 +73,7 @@ cart.forEach( cartItem => {
                 </div>
                 <div class="delivery-option">
                   <input type="radio" checked class="delivery-option-input"
-                    name="delivery-option-2">
+                    name="delivery-option-${productElement.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -84,7 +85,7 @@ cart.forEach( cartItem => {
                 </div>
                 <div class="delivery-option">
                   <input type="radio" class="delivery-option-input"
-                    name="delivery-option-2">
+                    name="delivery-option-${productElement.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
