c7a4a00e9a227783d2b186ad05fe57904acd5157
tomi-0 <asadetomi@gmail.com>
2025-08-29 14:15:41 +0100

    added interactivity to delivery option radios

--- data/cart.js
+++ data/cart.js
@@ -3,12 +3,6 @@
 // will either assign [] or current cart contents to cart array when page is reloaded
 export let cart = [] && JSON.parse(localStorage.getItem('cart'));
 
-cart = [{
-  productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  quantity: 1,
-  deliveryOptionId: 2,
-}];
-
 // saves cart's contents to local storage
 function saveToStorage() {
   localStorage.setItem('cart',JSON.stringify(cart));
@@ -61,6 +55,15 @@ export function updateQuantity(productId, newQuantity) {
   cart.forEach((cartItem) => {
     if (cartItem.productId === productId) {
       cartItem.quantity = newQuantity;
+    }
+  })
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      cartItem.deliveryOptionId = deliveryOptionId;
     }
   })
   saveToStorage();
