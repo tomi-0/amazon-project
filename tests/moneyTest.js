061969b828fef2380d643e506873c394f18b1298
tomi-0 <asadetomi@gmail.com>
2025-09-06 15:52:11 +0100

    created automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,38 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('Test suite: formatCurrency');
+
+console.log('converts cents into dollars');
+
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed')
+} else {
+    console.log('failed');
+}
+
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') {
+    console.log('passed')
+} else {
+    console.log('failed');
+}
+
+
+console.log('rounds up correctly to the nearest cent');
+
+if (formatCurrency(1000.5) === '10.01') {
+    console.log('passed')
+} else {
+    console.log('failed');
+}
+
+
+console.log('rounds down correctly to the nearest cent');
+
+if (formatCurrency(1000.4) === '10.00') {
+    console.log('passed')
+} else {
+    console.log('failed');
+}
