6d6b86016166a6be207b1cb19049ddecec71f3d7
tomi-0 <asadetomi@gmail.com>
2025-09-07 14:24:21 +0100

    add money tests using jamsine framework

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,24 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+// Reads like english
+// Creates a test suite
+describe('test suite: formatCurrency', () => {
+    // creates a test
+    it('converts cents into dollars', () => {
+        // code inside test
+        // lets us comapre a value to another value - object returned
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('rounds up correctly to the nearest cent', () => {
+        expect(formatCurrency(1000.5)).toEqual('10.01');
+    })
+
+    it('rounds down correctly to the nearest cent', () => {
+        expect(formatCurrency(1000.4)).toEqual('10.00');
+    })
+});
+ 
