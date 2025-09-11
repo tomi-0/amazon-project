4001d29cfc5d2fb5d7f2af6c882c43dc31d3042b
tomi-0 <asadetomi@gmail.com>
2025-09-11 15:09:24 +0100

    use inheritance and polymorphism to add a Clothing class

--- data/products.js
+++ data/products.js
@@ -33,6 +33,26 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
+
+}
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return`
+      <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
 }
 
 export const products = [
@@ -695,5 +715,8 @@ export const products = [
     ]
   }
 ].map( (product) => {
+  if (product.type === 'clothing') {
+    return new Clothing(product);
+  }
   return new Product(product);
 });
